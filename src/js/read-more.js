76a243412027146cf1aaa24d49ea99afb90d20a5
Olijnyks <101010916+oliinyks@users.noreply.github.com>
2022-08-26 10:14:09 +0300

    add read more

--- src/js/read-more.js
+++ src/js/read-more.js
@@ -1,19 +1,17 @@
 (() => {
-  const dots = document.querySelector('#para-dots');
+  //   const dots = document.querySelector('#para-dots');
   const morePara = document.querySelector('#more-para');
   const btnPara = document.querySelector('#btn');
 
   btnPara.addEventListener('click', onBtnClick);
 
   function onBtnClick() {
-    if (dots.style.display === 'none') {
-      dots.style.display = 'inline';
-      btnPara.innerHTML = 'Read More';
+    if (morePara.style.display === 'none') {
+      btnPara.textContent = 'Less';
       morePara.style.display = 'block';
     } else {
-      dots.style.display = 'none';
-      btnPara.innerHTML = 'Less';
-      morePara.style.display = 'inline';
+      btnPara.textContent = 'Read More';
+      morePara.style.display = 'none';
     }
   }
 })();
