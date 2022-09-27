455cfeb291f9af5fa7955e4ffaf33653ab242eb7
oliinyks <101010916+oliinyks@users.noreply.github.com>
2022-09-27 13:39:38 +0000

    Deploying to gh-pages from @ oliinyks/TasteEat@a8524a0f9ec7bea6eca9e08c615f069616b445ce 🚀

--- index.17a0b948.js
+++
@@ -1,2 +0,0 @@
-goToTopEl=document.getElementById("goToTop"),window.onscroll=function(){document.body.scrollTop>200||document.documentElement.scrollTop>200?goToTopEl.style.display="block":goToTopEl.style.display="none"},goToTopEl.addEventListener("click",(function(){document.body.scrollTop=0,document.documentElement.scrollTop=0}));
-//# sourceMappingURL=index.17a0b948.js.map
