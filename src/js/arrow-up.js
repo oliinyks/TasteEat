7eab1720fe9871d23aa52fc733f54b43a85d6d84
Olijnyks <101010916+oliinyks@users.noreply.github.com>
2022-09-27 17:50:09 +0300

    Update arrow-up.js

--- src/js/arrow-up.js
+++ src/js/arrow-up.js
@@ -1,19 +1,21 @@
-const goToTopEl = document.querySelector('#goToTop');
-const headerEl = document.querySelector('#header');
-
-function arrowScrollTop() {
-  if (window.scrollY > 50) {
-	goToTopEl.style.display = 'block';
-  } else {
-	goToTopEl.style.display = 'none';
-  }
-
-  goToTopEl.addEventListener('click', () => {
-	headerEl.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
-  });
-}
-
-window.addEventListener('scroll', arrowScrollTop);
+(() => {
+	const goToTopEl = document.querySelector('#goToTop');
+	const headerEl = document.querySelector('#header');
+	
+	function arrowScrollTop() {
+	  if (window.scrollY > 50) {
+		goToTopEl.style.display = 'block';
+	  } else {
+		goToTopEl.style.display = 'none';
+	  }
+	
+	  goToTopEl.addEventListener('click', () => {
+		headerEl.scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		 });
+	  });
+	}
+	
+	window.addEventListener('scroll', arrowScrollTop);
+})();
