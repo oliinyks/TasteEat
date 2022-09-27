15582c22e3e60d0e53b92858ee82f8da9dc82d97
oliinyks <101010916+oliinyks@users.noreply.github.com>
2022-09-27 14:07:22 +0000

    Deploying to gh-pages from @ oliinyks/TasteEat@309206733b17381a3ad800ea54ebb02f1b87b744 🚀

--- index.8ed17f61.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault(),e.currentTarget.reset()}));
-//# sourceMappingURL=index.8ed17f61.js.map
--- index.cdb4b319.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault(),e.currentTarget.reset()}));
-//# sourceMappingURL=index.cdb4b319.js.map
